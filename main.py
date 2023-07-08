ff806f49160e64da1b9d1a57624a1144dfd74f7a
Виталий Ильин <sima26ru@gmail.com>
2023-07-08 14:02:00 +0300

    добавление блока if main

--- main.py
+++ main.py
@@ -15,28 +15,33 @@ def get_years(eyars_old):
         return f'{eyars_old} лет'
 
 
-excel_data = pandas.read_excel('wine3.xlsx', na_filter=False)
-wines = excel_data.to_dict(orient='records')
-categories = defaultdict(list)
-for wine in wines:
-    categories[wine['Категория']].append(wine)
+def main():
+    excel_data = pandas.read_excel('wine3.xlsx', na_filter=False)
+    wines = excel_data.to_dict(orient='records')
+    categories = defaultdict(list)
+    for wine in wines:
+        categories[wine['Категория']].append(wine)
 
-pprint.pprint(categories)
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-delta = (datetime.datetime.now() - datetime.datetime(year=1920, month=1, day=1))
-years = delta.days // 365
-template = env.get_template('wine.html')
-yers_old = get_years(years)
-rendered_page = template.render(
-    years_old=yers_old,
-    categories=categories
-)
+    pprint.pprint(categories)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    delta = (datetime.datetime.now() - datetime.datetime(year=1920, month=1, day=1))
+    years = delta.days // 365
+    template = env.get_template('wine.html')
+    yers_old = get_years(years)
+    rendered_page = template.render(
+        years_old=yers_old,
+        categories=categories
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
